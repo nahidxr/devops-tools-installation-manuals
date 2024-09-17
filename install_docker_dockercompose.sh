0e86a920ce604d0154ec971fb8e993d21ad61946
Md Nahidul Islam <91475017+nahidxr@users.noreply.github.com>
2024-09-17 13:27:37 +0600

    Update install_docker_dockercompose.sh

--- install_docker_dockercompose.sh
+++ install_docker_dockercompose.sh
@@ -38,7 +38,7 @@ source /etc/profile
 echo "Docker installation completed."
 
 # Installing Docker Compose
-sudo curl -SL https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/2.29.3/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
 
 # Give execute permissions to Docker Compose
 sudo chmod +x /usr/bin/docker-compose
